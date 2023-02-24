ba0320db0b5db934512da9b7cc71c164cdff0355
Dulniko <jakub.dulnik03@gmail.com>
2023-02-24 17:12:11 +0100

    sorting by searching and ordering

--- zadanie/tasks/views.py
+++ zadanie/tasks/views.py
@@ -1,7 +1,17 @@
 from rest_framework import viewsets
+from rest_framework import filters
+
 from .models import Task
 from .serializers import TaskSerializer
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description']
+    ordering_fields = ['id', 'name', 'description', 'status', 'assigned_user']
+
+    
+
