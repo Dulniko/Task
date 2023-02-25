dc6e61ac7e14ef93ab3a61f34474657067793155
Dulniko <jakub.dulnik03@gmail.com>
2023-02-25 11:58:15 +0100

    filter for 'assigned_user', 'status'

--- zadanie/tasks/views.py
+++ zadanie/tasks/views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets
-from rest_framework import filters
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -9,9 +9,13 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
     search_fields = ['name', 'description']
     ordering_fields = ['id', 'name', 'description', 'status', 'assigned_user']
+    filterset_fields = ['assigned_user', 'status'] 
+
+    
+
 
     
 
